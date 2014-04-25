eaa49faa04ab1480f9e5087d0d3a19ecb7b9859c
Zeno Zeng <zenoofzeng@gmail.com>
2014-04-25 23:05:18 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 type xbindkeys >/dev/null 2>&1 || { echo >&2 "xbindkeys not installed, aborting."; exit 1; }
 type xdotool >/dev/null 2>&1 || { echo >&2 "xdotool not installed, aborting."; exit 1; }
 mkdir -p ~/.dotxbindkeys
-cp *.scm ~/.dotxbindkeys
-cp bin/* ~/.local/bin
+cp -i scheme/* ~/.dotxbindkeys
+yes | cp -f bin/* ~/.local/bin
