bcdbfb62e389f094c9c1608c3382b7e5d1997351
Zeno Zeng <zenoofzeng@gmail.com>
2014-04-27 22:54:31 +0800

    update init.scm

--- init.scm
+++ init.scm
@@ -1,20 +1,20 @@
 (use-modules (ice-9 ftw))
 
-(load "keymap.scm")
-(load "message.scm")
-(load "functions.scm")
+(load ".dotxbindkeys/lib/keymap.scm")
+(load ".dotxbindkeys/lib/message.scm")
+(load ".dotxbindkeys/lib/functions.scm")
 
 ;; load plugins
 (map (lambda (scm)
        (display (string-append "Loading Plugin: " scm "\n"))
        (load scm))
      (map (lambda (child)
-            (string-append "plugins/" (car child)
+            (string-append ".dotxbindkeys/plugins/" (car child)
                            "/" (car child)
                            ".scm"))
           (cdr (cdr (file-system-tree
                      (string-append (dirname (current-filename))
-                                    "/plugins"))))))
+                                    ".dotxbindkeys/plugins"))))))
 
 ;; include user's scheme
-(load "user.scm")
+(load ".dotxbindkeys/user/user.scm")
