a670a537f86550a9638bc1f78c916d88c23f0e9c
Zeno Zeng <zenoofzeng@gmail.com>
2014-04-27 23:44:49 +0800

    use association lists in keymap.scm

--- lib/keymap.scm
+++ lib/keymap.scm
@@ -1,26 +1,39 @@
 (define keybindings '())
 
 (define (define-key keymap key binding)
-  (set! keybindings (append keybindings '((keymap key binding)))))
+  (define keybinding '())
+  (set! keybinding (acons "keymap" keymap keybinding))
+  (set! keybinding (acons "key" key keybinding))
+  (set! keybinding (acons "binding" binding keybinding))
+  (set! keybindings (append keybindings (list keybinding))))
 
 (define (set-keymap keymap)
 
   (define active-keybindings
     (filter (lambda (keybinding)
-              (or (equal? (car keybinding) keymap)
-                  (equal? (car keybinding) "global")))
+              (or (equal? (assoc-ref keybinding "keymap")
+                          keymap)
+                  (equal? (assoc-ref keybinding "keymap")
+                          "global")))
             keybindings))
+
   (define active-keys
-    (delete-duplicates (map car active-keybindings)))
+    (delete-duplicates (map (lambda (keybinding)
+                              (assoc-ref keybinding "key"))
+                            active-keybindings)))
 
   (define (apply-callbacks key)
-    (map display "APPLY CALLBACKS: " key "\n")
+    (map display (list "APPLY CALLBACKS: " key "\n"))
     (map (lambda (keybinding)
-           ((thrid keybinding)))
+           ((assoc-ref keybinding "binding")))
          (filter (lambda (keybinding)
-                   (equal? key (second keybinding)))
+                   (equal? key (assoc-ref keybinding "key")))
                  active-keybindings)))
 
+  (map display (list "Switch to keymap: " keymap "\n"))
+
+  (display keybindings)
+  
   (ungrab-all-keys)
   (remove-all-keys)
   (map (lambda (key)
